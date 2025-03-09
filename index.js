eb89cb16fe0ba7e1a823627298487bf8b1dc1549
Aginas <aginasnathan2109@gmail.com>
2025-03-09 15:03:42 +0530

    index commit

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 let items = [
-  { id: 1, title: "Buy milk" },
+  { id: 1, title: "Buy Chocolate" },
   { id: 2, title: "Finish homework" },
 ];
 
